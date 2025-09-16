faf74d430d806af8e02ba4d9438ea4cf1bf18383
Daniil Ignatjev <daniil.ignatjev@gmail.com>
2025-09-16 21:36:50 +0300

    Start commit

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
+
+export default defineConfig([
+	globalIgnores(['dist']),
+	{
+		files: ['**/*.{js,jsx,ts,tsx}'],
+		extends: [js.configs.recommended, reactHooks.configs['recommended-latest'], reactRefresh.configs.vite],
+		languageOptions: {
+			ecmaVersion: 'latest',
+			globals: globals.browser,
+			parserOptions: {
+				ecmaVersion: 'latest',
+				ecmaFeatures: { jsx: true },
+				sourceType: 'module'
+			}
+		},
+		rules: {
+			semi: ['error', 'always'],
+			'no-unused-vars': 'off',
+			'no-undef': 'warn',
+			'prefer-const': 'off'
+		}
+	}
+]);
