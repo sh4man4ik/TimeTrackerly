01c864272102ed955a0e877594ddd7fa7562dbf5
Daniil Ignatjev <daniil.ignatjev@gmail.com>
2025-09-20 18:27:38 +0300

    Navbar title position fix

--- src/modules/Navbar.tsx
+++ src/modules/Navbar.tsx
@@ -12,7 +12,7 @@ function Navbar() {
 
 	return (
 		<>
-			<div className="bg-[#f8f8ff] flex flex-wrap justify-between mx-auto pt-[14px] pb-[14px] pl-[19px] pr-[19px] lg:pt-[28px] lg:pb-[28px] lg:pl-[38px] lg:pr-[38px] shadow-lg/4">
+			<div className="bg-[#f8f8ff] flex flex-wrap items-center justify-between mx-auto pt-[14px] pb-[14px] pl-[19px] pr-[19px] lg:pt-[28px] lg:pb-[28px] lg:pl-[38px] lg:pr-[38px] shadow-lg/4">
 				<NavbarTitle></NavbarTitle>
 
 				<div onClick={handleMenu}>
