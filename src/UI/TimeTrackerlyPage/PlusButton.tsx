77d06fbb58d7d4880f453bbef6b4f4dba315e208
Daniil Ignatjev <daniil.ignatjev@gmail.com>
2025-09-22 22:21:27 +0300

    Fixed title and start activity button position on small devices

--- src/UI/TimeTrackerlyPage/PlusButton.tsx
+++ src/UI/TimeTrackerlyPage/PlusButton.tsx
@@ -1,7 +1,7 @@
 function StartActivityButton() {
 	return (
 		<>
-			<button className="bg-gradient-to-r from-[#7474BF] to-[#348AC7] hover:from-[#6b6bb0] hover:to-[#3180b0] rounded-[6px] lg:rounded-[12px] size-[35px] lg:size-[70px] ml-[15px] lg:ml-[30px] text-3xl lg:text-6xl font-bold text-[#F8F8FF] flex items-center justify-center pb-[4px] lg:pb-[10px] cursor-pointer">
+			<button className="bg-gradient-to-r from-[#7474BF] to-[#348AC7] hover:from-[#6b6bb0] hover:to-[#3180b0] rounded-[6px] lg:rounded-[12px] size-[35px] lg:size-[70px] text-3xl lg:text-6xl font-bold text-[#F8F8FF] flex items-center justify-center pb-[4px] lg:pb-[10px] cursor-pointer">
 				+
 			</button>
 		</>
--- src/components/TimeTrackerlyPage/StartActivity.tsx
+++ src/components/TimeTrackerlyPage/StartActivity.tsx
@@ -4,7 +4,7 @@ import PlusButton from '../../UI/TimeTrackerlyPage/PlusButton';
 function StartActivity() {
 	return (
 		<>
-			<div className="flex mt-[45px] lg:mt-[90px] items-center justify-center">
+			<div className="flex flex-wrap mt-[45px] lg:mt-[90px] items-center justify-center gap-[10px] lg:gap-[30px] ml-[20px] mr-[20px] text-center">
 				<StartActivityTitle></StartActivityTitle>
 				<PlusButton></PlusButton>
 			</div>
