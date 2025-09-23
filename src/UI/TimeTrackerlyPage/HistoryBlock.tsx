1a17b3871a6360c8fec4481fdea97712560b07d9
Daniil Ignatjev <daniil.ignatjev@gmail.com>
2025-09-23 15:38:31 +0300

    Added margin bottom to 7 days history in main page

--- src/UI/TimeTrackerlyPage/HistoryBlock.tsx
+++ src/UI/TimeTrackerlyPage/HistoryBlock.tsx
@@ -1,7 +1,7 @@
 function HistoryBlock() {
 	return (
 		<>
-			<div className="text-base lg:text-2xl p-[12px] lg:p-[25px] bg-gradient-to-r from-[#7474BF]/20 to-[#348AC7]/20 rounded-[6px] lg:rounded-[12px] mt-[17px] lg:mt-[35px] max-w-[450px] lg:max-w-[900px] w-[90%] h-auto justify-self-center grid gap-[7px] lg:gap-[15px]">
+			<div className="text-base lg:text-2xl p-[12px] lg:p-[25px] bg-gradient-to-r from-[#7474BF]/20 to-[#348AC7]/20 rounded-[6px] lg:rounded-[12px] mt-[17px] lg:mt-[35px] max-w-[450px] lg:max-w-[900px] w-[90%] h-auto justify-self-center grid gap-[7px] lg:gap-[15px] mb-[30px] lg:mb-[50px]">
 				<p className="font-bold">Today</p>
 				<ul>
 					<li>17:30 - 22:49 playing computer games</li>
