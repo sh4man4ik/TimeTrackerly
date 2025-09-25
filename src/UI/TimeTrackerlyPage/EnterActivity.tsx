174c5b55cad4337f13ecd6e99c76c39e54f7874f
Daniil Ignatjev <daniil.ignatjev@gmail.com>
2025-09-25 16:00:13 +0300

    Max length of activity 30

--- src/UI/TimeTrackerlyPage/EnterActivity.tsx
+++ src/UI/TimeTrackerlyPage/EnterActivity.tsx
@@ -6,7 +6,7 @@ function EnterActivity() {
 				<input
 					type="text"
 					className="rounded-[6px] lg:rounded-[12px] bg-gradient-to-r from-[#7474BF] to-[#348AC7] p-[2px] lg:p-[8px] pl-[8px] lg:pl-[16px] pr-[8px] lg:pr-[16px] w-[100%] max-w-[500px] outline-none hover:from-[#6b6bb0] hover:to-[#3180b0] text-[#F8F8FF] placeholder-[#F8F8FF]"
-					maxLength={100}
+					maxLength={30}
 					placeholder="Go to training..."
 				/>
 			</div>
