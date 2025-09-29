ca1f54456dfc667de573927bc6f24f3743f5c451
Daniil Ignatjev <daniil.ignatjev@gmail.com>
2025-09-29 21:06:24 +0300

    Added control of empty list in history

--- src/UI/HistoryBlock.tsx
+++ src/UI/HistoryBlock.tsx
@@ -2,71 +2,29 @@ function HistoryBlock(props: any) {
 	return (
 		<>
 			<div className="text-base lg:text-2xl p-[12px] lg:p-[25px] bg-gradient-to-r from-[#7474BF]/20 to-[#348AC7]/20 rounded-[6px] lg:rounded-[12px] mt-[17px] lg:mt-[35px] max-w-[450px] lg:max-w-[900px] w-[90%] h-auto justify-self-center grid gap-[7px] lg:gap-[15px] mb-[30px] lg:mb-[50px]">
-				{props.activityData.map((activity: any, index: any) => (
-					<div key={index}>
-						<p>{activity.trim()}</p>
-					</div>
-				))}
-				{props.historytype == 'full' ? (
-					<div className="grid gap-[7px] lg:gap-[15px]">
-						<p className="font-bold">17.09.2025</p>
-						<ul>
-							<li>17:30 - 22:49 playing computer games</li>
-							<li>08:00 - 10:00 breakfast</li>
-							<li>07:00 - 08:00 sleeping</li>
-						</ul>
+				{props.activityData.length !== 0 ? (
+					<>
+						{props.activityData.map((activity: any, index: any) => (
+							<div key={index}>
+								<p>{activity.trim()}</p>
+							</div>
+						))}
 
-						<p className="font-bold">16.09.2025</p>
-						<ul>
-							<li>17:30 - 22:49 playing computer games</li>
-							<li>08:00 - 10:00 breakfast</li>
-							<li>07:00 - 08:00 sleeping</li>
-						</ul>
-
-						<p className="font-bold">15.09.2025</p>
-						<ul>
-							<li>17:30 - 22:49 playing computer games</li>
-							<li>08:00 - 10:00 breakfast</li>
-							<li>07:00 - 08:00 sleeping</li>
-						</ul>
-
-						<p className="font-bold">14.09.2025</p>
-						<ul>
-							<li>17:30 - 22:49 playing computer games</li>
-							<li>08:00 - 10:00 breakfast</li>
-							<li>07:00 - 08:00 sleeping</li>
-						</ul>
-
-						<p className="font-bold">12.09.2025</p>
-						<ul>
-							<li>17:30 - 22:49 playing computer games</li>
-							<li>08:00 - 10:00 breakfast</li>
-							<li>07:00 - 08:00 sleeping</li>
-						</ul>
-
-						<p className="font-bold">10.09.2025</p>
-						<ul>
-							<li>17:30 - 22:49 playing computer games</li>
-							<li>08:00 - 10:00 breakfast</li>
-							<li>07:00 - 08:00 sleeping</li>
-						</ul>
-
-						<p className="font-bold">09.09.2025</p>
-						<ul>
-							<li>17:30 - 22:49 playing computer games</li>
-							<li>08:00 - 10:00 breakfast</li>
-							<li>07:00 - 08:00 sleeping</li>
-						</ul>
-
-						<p className="font-bold">01.09.2025</p>
-						<ul>
-							<li>17:30 - 22:49 playing computer games</li>
-							<li>08:00 - 10:00 breakfast</li>
-							<li>07:00 - 08:00 sleeping</li>
-						</ul>
-					</div>
+						{props.historytype == 'full' ? (
+							<div className="grid gap-[7px] lg:gap-[15px]">
+								<p className="font-bold">17.09.2025</p>
+								<ul>
+									<li>17:30 - 22:49 full</li>
+									<li>08:00 - 10:00 full</li>
+									<li>07:00 - 08:00 full</li>
+								</ul>
+							</div>
+						) : (
+							<></>
+						)}
+					</>
 				) : (
-					<></>
+					<p>The list is empty :(</p>
 				)}
 			</div>
 		</>
