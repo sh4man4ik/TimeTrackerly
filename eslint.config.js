06d5b8e2b9270b582199bb01ec562089177fe6b8
Daniil Ignatjev <daniil.ignatjev@gmail.com>
2025-10-17 15:50:20 +0300

    Better eslint 2

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,6 @@ import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import typescript from '@typescript-eslint/eslint-plugin';
 import parser from '@typescript-eslint/parser';
 import { defineConfig, globalIgnores } from 'eslint/config';
 
