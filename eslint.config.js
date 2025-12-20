c83de67b128e1f6fd7cf69191a547dfff11f7bc8
Daniil Ignatjev <daniil.ignatjev@gmail.com>
2025-12-21 00:37:46 +0200

    eslint updates

--- eslint.config.js
+++ eslint.config.js
@@ -1,27 +1,38 @@
 import js from '@eslint/js';
-import globals from 'globals';
+import parser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import parser from '@typescript-eslint/parser';
+import globals from 'globals';
 import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
 	globalIgnores(['dist']),
+
 	{
 		files: ['**/*.{js,jsx,ts,tsx}'],
+
 		extends: [js.configs.recommended, reactHooks.configs['recommended-latest'], reactRefresh.configs.vite],
+
 		languageOptions: {
-			parser: parser,
+			parser,
 			ecmaVersion: 'latest',
 			globals: globals.browser,
 			parserOptions: {
 				ecmaVersion: 'latest',
-				ecmaFeatures: { jsx: true },
-				sourceType: 'module'
+				sourceType: 'module',
+				ecmaFeatures: { jsx: true }
 			}
 		},
+
+		plugins: {
+			'@typescript-eslint': tsPlugin
+		},
+
 		rules: {
+			semi: ['error', 'always'],
 			'no-unused-vars': 'off',
+			'@typescript-eslint/no-unused-vars': 'warn',
 			'no-undef': 'warn',
 			'prefer-const': 'off'
 		}
